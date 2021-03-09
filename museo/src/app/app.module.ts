cc7b4d0b219add99855a0e12da0e591c8043c26c
Louis-Guiniot <louis99679@gmail.com>
2021-03-09 23:07:35 +0100

    fix con nmp i

--- museo/src/app/app.module.ts
+++ museo/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +10,12 @@ import { ArticoliComponent } from './features/articoli/main/articoli.component';
 import { HomeComponent } from './features/home/main/home.component';
 import { LoginComponent } from './features/login/main/login.component';
 import { PageNotFoundComponent } from './features/page-not-found/main/page-not-found.component';
-
+import { reducers } from './redux';
+import { ArticoloEffects } from './redux/redux-articolo/redux-articolo.effects';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+import { HttpCommunicationsService } from 'src/core/http/http-communications.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,9 +27,17 @@ import { PageNotFoundComponent } from './features/page-not-found/main/page-not-f
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    StoreModule.forRoot(reducers),
+     EffectsModule.forRoot([
+      ArticoloEffects
+     ]),
+     NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpCommunicationsService],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
 })
 export class AppModule { }
