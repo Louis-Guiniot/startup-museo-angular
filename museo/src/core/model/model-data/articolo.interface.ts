466389dbf4918eb4554016c2d72bde887db0addf
Louis-Guiniot <louis99679@gmail.com>
2021-03-09 23:06:03 +0100

    create interfacce response e articolo

---
+++ museo/src/core/model/model-data/articolo.interface.ts
@@ -0,0 +1,15 @@
+export interface Articolo{
+    
+    id?:number
+    modello:string
+    marca:string
+    descrizione:string
+    nazionalita:string
+    ram:string
+    processore:string
+    schedaMadre:string
+    schedaVideo:string
+    annoProduzioneInizio:string
+    annoProduzioneFine:string
+    foto: string
+}
