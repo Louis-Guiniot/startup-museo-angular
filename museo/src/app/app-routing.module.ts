3ba16e35180b9637fb404fa70ce8e75060c0db91
Louis-Guiniot <louis99679@gmail.com>
2021-03-09 21:47:07 +0100

    aggiornati app module e app routing

--- museo/src/app/app-routing.module.ts
+++ museo/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  //prime pagine visibili
+  {path:'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)},
+  {path:'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)},
+
+  {path:'museo', children: [
+      {path:'articoli', loadChildren: () => import('./features/articoli/articoli.module').then(m => m.ArticoliModule)}
+    ]
+  },
+
+  //pagine di errore e login non fatta
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadChildren: () => import('./features/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- museo/src/app/app.module.ts
+++ museo/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticoliComponent } from './features/articoli/main/articoli.component';
+import { HomeComponent } from './features/home/main/home.component';
+import { LoginComponent } from './features/login/main/login.component';
+import { PageNotFoundComponent } from './features/page-not-found/main/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ArticoliComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
