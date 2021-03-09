5748cedd9d42bbfb3683cd1fae64b16667af4269
Louis-Guiniot <louis99679@gmail.com>
2021-03-09 23:06:19 +0100

    prima stampa articolo a video

--- museo/src/app/features/articoli/main/articoli.component.ts
+++ museo/src/app/features/articoli/main/articoli.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectArticolis } from 'src/app/redux/redux-articolo';
+import { Articolo } from 'src/core/model/model-data/articolo.interface';
+import { ArticoliService } from '../services/articoli.service';
 
 @Component({
   selector: 'app-articoli',
@@ -7,9 +14,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticoliComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private articoliService: ArticoliService, private store: Store, private fb: FormBuilder) { 
+    this.articoliService.retreiveAllArticoli()
+  }
 
   ngOnInit(): void {
   }
 
+  get lista_articoli(): Observable<Articolo[]> {
+    return this.store.pipe(select(selectArticolis))
+  }
+  
 }
