8d0e23988d9fbdda3c9a866269372fc37327b86a
Louis-Guiniot <louis99679@gmail.com>
2021-03-09 23:38:30 +0100

    aggiunti commenti

--- museo/src/app/features/articoli/main/articoli.component.ts
+++ museo/src/app/features/articoli/main/articoli.component.ts
@@ -14,13 +14,15 @@ import { ArticoliService } from '../services/articoli.service';
 })
 export class ArticoliComponent implements OnInit {
 
-  constructor(private router: Router, private articoliService: ArticoliService, private store: Store, private fb: FormBuilder) { 
+  constructor(private router: Router, private articoliService: ArticoliService, private store: Store, private fb: FormBuilder) {
+    //tramite service carico già la lista di articoli. da Undefined però
     this.articoliService.retreiveAllArticoli()
   }
 
   ngOnInit(): void {
   }
 
+  //tramite redux prendo lista articoli presenti su db. su html accedo tramite for di lista_articoli
   get lista_articoli(): Observable<Articolo[]> {
     return this.store.pipe(select(selectArticolis))
   }
