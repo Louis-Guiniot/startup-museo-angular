40694e0f93c843595e003b3fa1c376a4966a25f1
Louis-Guiniot <louis99679@gmail.com>
2021-03-11 00:01:28 +0100

    aggiunti state e reducer a index

--- museo/src/app/redux/index.ts
+++ museo/src/app/redux/index.ts
@@ -1,14 +1,16 @@
 import { routerReducer, RouterReducerState } from "@ngrx/router-store";
 import { ActionReducerMap } from "@ngrx/store";
 import { ArticoloState, articoloReducer } from "./redux-articolo/redux-articolo.reducers";
+import { utenteReducer, UtenteState } from "./redux-utente/redux-user.reducers";
 
 export interface AppState{
     router: RouterReducerState<any>;
     articoloState: ArticoloState
-
+    utenteState: UtenteState
 }
 
 export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer,
-    articoloState: articoloReducer
+    articoloState: articoloReducer,
+    utenteState: utenteReducer
 }
