ec990b407dd01ff028d72db626089dfa1213947f
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-04-22 15:47:47 +0200

    aggiornate routes

--- museo/src/app/app-routing.module.ts
+++ museo/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -11,15 +12,14 @@ const routes: Routes = [
     {path:'user', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)},
   ]},
 
-  {path:'museo', children: [
+ 
       {path:'articoli', loadChildren: () => import('./features/articoli/articoli.module').then(m => m.ArticoliModule)},
       {path:'articoli/preferiti', loadChildren: () => import('./features/preferiti/preferiti.module').then(m => m.PreferitiModule)},
-      {path:'dettaglio/articolo/:id', loadChildren: () =>import('./features/dettaglio-articolo/dettaglio-articolo.module').then(m => m.DettaglioArticoloModule)}
-    ]
-  },
+      {path:'dettaglio/articolo/:id', loadChildren: () => import('./features/dettaglio-articolo/dettaglio-articolo.module').then(m => m.DettaglioArticoloModule)},
+      {path:'crea/articolo', loadChildren: () => import('./features/crea-articolo/crea-articolo.module').then(m => m.CreaArticoloModule)},
 
   //pagine di errore e login non fatta
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/articoli', pathMatch: 'full' },
   
   { path: 'cards', loadChildren: () => import('./features/cards/cards.module').then(m => m.CardsModule) },
   
@@ -32,7 +32,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
